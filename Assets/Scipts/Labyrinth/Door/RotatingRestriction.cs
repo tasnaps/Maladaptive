624675afe13d39d83794b831952a30acf2fc8c59
Tapio Meriläinen <74553493+tasnaps@users.noreply.github.com>
2023-05-10 18:42:42 +0300

    Removed sourcecode

--- Assets/Scipts/Labyrinth/Door/RotatingRestriction.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class RotatingRestriction : MonoBehaviour
-{
-    [SerializeField] public int restrictionMin = 0;
-    [SerializeField] public int restrictionMax = 360;
-}
